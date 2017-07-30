993847df98ad2f19c81439c4147ed82810119eb6
davisk4rpi <davisk4rpi@gmail.com>
2017-07-30 03:38:53 -0700

    add my reps page

--- src/components/rep.js
+++ src/components/rep.js
@@ -1,9 +1,60 @@
 import React, {Component} from 'react';
+import { Card, Image, List } from 'semantic-ui-react';
 
 export default class Rep extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: this.props.data
+    };
+  }
+
   render() {
+    const {phone, twitter, email, location, position, name, imgSrc} = this.state.data;
+    let twitterContent;
+    if (twitter) {
+      twitterContent = (
+        <List.Item>
+          <List.Icon name='twitter' />
+          <List.Content>
+            <a href={`https://twitter.com/${twitter}`}>@{twitter}</a>
+          </List.Content>
+        </List.Item>
+      );
+    }
+    const descriptionComponent = (
+      <Card.Description>
+        <List>
+          <List.Item>
+            <List.Icon name='phone' />
+            <List.Content>{phone}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='mail' />
+            <List.Content>
+              <a href={`mailto:${email}`}>{email}</a>
+            </List.Content>
+          </List.Item>
+          {twitterContent}
+        </List>
+      </Card.Description>
+    );
+
     return (
-      <div></div>
-    )
+      <Card fluid id="rep-card">
+        <Card.Content>
+          <Image src={imgSrc} floated='left' shape="circular" size='tiny'/>
+          <Card.Header>
+            {name}
+          </Card.Header>
+          <Card.Meta>
+            {position}, {location}
+          </Card.Meta>
+          {descriptionComponent}
+        </Card.Content>
+      </Card>
+    );
   }
 }
--- src/components/rep_container.js
+++ src/components/rep_container.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
+import { Header } from 'semantic-ui-react';
+
+import Rep from './rep';
+import database from '../database';
 
 export default class RepContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      officials: {},
+    };
+  }
+
+  componentDidMount() {
+    let officials;
+    database.ref('/officials').once('value').then( snap => {
+      officials = snap.val();
+      this.setState({ officials });
+    });
+  }
+
   render() {
+    const {officials} = this.state;
+    let officialId, officialObj;
+    const officialComponents = _.map(officials, (value, key) => {
+      officialId = key;
+      officialObj = value;
+      return (
+        <Rep
+          key={officialId}
+          data={officialObj}
+        />
+      );
+    });
+
     return (
-      <div></div>
-    )
+      <div>
+        <Header as="h2">My Representatives</Header>
+        {officialComponents}
+      </div>
+    );
   }
 }
