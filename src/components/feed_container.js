069b039f0e16890d55310b143917e4c9f2b85519
davisk4rpi <davisk4rpi@gmail.com>
2017-07-29 21:10:13 -0700

    committing incomplete feed container

--- src/components/feed_container.js
+++ src/components/feed_container.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
+import { Button } from 'semantic-ui-react';
+import Sidenav from './sidenav';
 
 export default class FeedContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { navVisible: false };
+    this.closeNav = this.closeNav.bind(this);
+  }
+
+  toggleVisibility = () => this.setState({ navVisible: !this.state.navVisible })
+
+  closeNav() {
+    if (this.state.navVisible) {
+      this.setState({navVisible: false});
+    }
+  }
+
   render() {
+    const {navVisible} = this.state;
     return (
-      <div></div>
+      <div onClick={navVisible ? this.closeNav : undefined}>
+        <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+        <Sidenav visible={navVisible}/>
+      </div>
     );
   }
 }
